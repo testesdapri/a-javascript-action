ee68f38babcc5b6d0ce0d747f4743179e3c8c2c3
testesdapri <75193656+testesdapri@users.noreply.github.com>
2021-04-04 22:30:27 -0500

    creates logic

---
+++ index.js
@@ -0,0 +1,14 @@
+const core = require("@actions/core");
+
+const firstGreeting = core.getInput("first-greeting");
+const secondGreeting = core.getInput("second-greeting");
+const thirdGreeting = core.getInput("third-greeting");
+
+console.log(`Hello ${firstGreeting}`);
+if (secondGreeting) {
+    console.log(`Hello ${secondGreeting}`);
+    core.setOutput(“last-one-greeted”); 
+} else if (thirdGreeting) {
+    console.log(`Hello ${thirdGreeting}`);
+    core.setOutput(“last-one-greeted”);
+}
