9de21cc0b63ea87ea9eff0e13f02b0048d38aee5
testesdapri <75193656+testesdapri@users.noreply.github.com>
2021-04-10 12:01:45 -0500

    fixes many things

--- .github/actions/hello-world/index.js
+++ .github/actions/hello-world/index.js
@@ -4,16 +4,18 @@ const github = require("@actions/github");
 const firstGreeting = core.getInput("first-greeting");
 const secondGreeting = core.getInput("second-greeting");
 const thirdGreeting = core.getInput("third-greeting");
-const lastOneGreeted = core.setOutput("last-one-greeted");
 
 console.log(`Hello ${firstGreeting}`);
-if (secondGreeting) {
-    console.log(`Hello ${secondGreeting}`);
-    // core.setOutput(“lastOneGreeted”); 
-} else if (thirdGreeting) {
-    console.log(`Hello ${thirdGreeting}`);
-    // core.setOutput(“lastOneGreeted”);
-}
-catch (error) {
+try {
+    if (firstGreeting) {
+        console.log(`Hello ${firstGreeting}`);
+        core.setOutput(“last-one-greeted”, firstGreeting); 
+    } else if (secondGreeting) {
+        console.log(`Hello ${secondGreeting}`);
+        core.setOutput(“last-one-greeted”, secondGreeting);
+    } else if (thirdGreeting) {
+        console.log(`Hello ${thirdGreeting}`);
+        core.setOutput(“last-one-greeted”, thirdGreeting);
+} catch (error) {
   core.setFailed(error.message);
 }
