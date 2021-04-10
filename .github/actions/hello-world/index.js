67902e28dc8497ed2cbffe5a584dcf86c5ecaeb6
testesdapri <75193656+testesdapri@users.noreply.github.com>
2021-04-10 12:33:37 -0500

    adds async function

--- .github/actions/hello-world/index.js
+++ .github/actions/hello-world/index.js
@@ -6,19 +6,23 @@ const secondGreeting = core.getInput("second-greeting");
 const thirdGreeting = core.getInput("third-greeting");
 const lastOneGreeted = core.getInput("last-one-greeted");
 
-try {
-    if (firstGreeting) {
-        console.log(`The last one greeted was ${firstGreeting}!`);
-        core.setOutput("last-one-greeted", firstGreeting); 
-    } else if (secondGreeting) {
-        console.log(`The last one greeted was ${secondGreeting}!`);
-        core.setOutput("last-one-greeted", secondGreeting);
-    } else if (thirdGreeting) {
-        console.log(`The last one greeted was ${thirdGreeting}!`);
-        core.setOutput("last-one-greeted", thirdGreeting);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+    try {
+        if (firstGreeting) {
+            console.log(`The last one greeted was ${firstGreeting}!`);
+            core.setOutput("last-one-greeted", firstGreeting); 
+        } else if (secondGreeting) {
+            console.log(`The last one greeted was ${secondGreeting}!`);
+            core.setOutput("last-one-greeted", secondGreeting);
+        } else if (thirdGreeting) {
+            console.log(`The last one greeted was ${thirdGreeting}!`);
+            core.setOutput("last-one-greeted", thirdGreeting);
+    } catch (error) {
+      core.setFailed(error.message);
+    }
 }
+
+run();
 
 
 //try {
